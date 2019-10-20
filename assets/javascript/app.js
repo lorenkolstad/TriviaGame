cd452a1571b7db2ed99ef507d39c807738397bc3
Loren Kolstad <lorenkolstad@gmail.com>
2019-10-19 20:28:13 -0700

    Got the background image to work. Added the timer. Fixed the hide/show functions for the different divs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
-// var questions = [""];
 
+
+var time = 0;
+var intervalId
 
 
 $("#startContainer").hide();
@@ -11,6 +13,12 @@ $("#startGame").on("click", function (event){
     event.preventDefault();
     $("#startContainer").show();
     $("#buttonContainer").hide();
+    setInterval(() => {
+        time+=1;
+        $("#timer").text("Time: " + time);
+        console.log(time);
+    }, 1000);
+    
 
 })
 
@@ -35,7 +43,7 @@ $("#submitAnswers").on("click", function (event){
     console.log(event);
     console.log(correctAnswers);
     console.log(wrongAnswers);
-    alert("You guessed " + correctAnswers + " questions correctly!")
+    // alert("You guessed " + correctAnswers + " questions correctly!")
 
 })
 
@@ -57,3 +65,36 @@ function endGame() {
 //     $("#buttonContainer").hide(); // can hide the questions and start container. Still need to change id tags.
 
 // })
+
+
+// function count() {
+
+//     // DONE: increment time by 1, remember we cant use "this" here.
+//     time++;
+    
+//     // DONE: Get the current time, pass that into the timeConverter function,
+//     //       and save the result in a variable.
+//     var converted = timeConverter(time);
+//     console.log(converted);
+    
+//     // DONE: Use the variable we just created to show the converted time in the "display" div.
+//     $("#display").text(converted);
+//     }
+//     function timeConverter(t) {
+
+//         var minutes = Math.floor(t / 60);
+//         var seconds = t - (minutes * 60);
+      
+//         if (seconds < 10) {
+//           seconds = "0" + seconds;
+//         }
+      
+//         if (minutes === 0) {
+//           minutes = "00";
+//         }
+//         else if (minutes < 10) {
+//           minutes = "0" + minutes;
+//         }
+      
+//         return minutes + ":" + seconds;
+//       }
