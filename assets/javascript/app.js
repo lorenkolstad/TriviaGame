e51263d65dd324d719809068a45b11d92cba568d
Loren Kolstad <lorenkolstad@gmail.com>
2019-10-19 23:53:12 -0700

    Created a timer that counts down from 30 seconds and stops the function if the clock hits zero or if the user clicks submit answers. Made the font size of the end results larger.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 
 
-var time = 0;
+var time = 30;
 var intervalId;
 
 
@@ -14,42 +14,29 @@ $("#startGame").on("click", function (event){
     event.preventDefault();
     $("#startContainer").show();
     $("#buttonContainer").hide();
-    setInterval(() => {
-        time+=1;
-        $("#timer").text("Time: " + time);
-        console.log(time);
-    }, 1000);
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
 
-    // DONE: increment time by 1, remember we cant use "this" here.
-    // time++;
-    
-    // DONE: Get the current time, pass that into the timeConverter function,
-    //       and save the result in a variable.
-    var converted = timeConverter(time);
-    console.log(converted);
+})
 
-function timeConverter(t) {
+// setInterval(() => {
+//     time--;
+//     $("#timer").text("Time remaining: " + time);
+//     console.log(time);
+// }, 1000);
+function decrement() {
+    time--;
+    $("#timer").text("Seconds remaining: " + time);
+    console.log(time);
 
-    // DONE: Use the variable we just created to show the converted time in the "display" div.
-    $("#timer").text(converted);
+    if (time === 0) {
+        endGame();
     }
 
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
-    
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-    
-    if (minutes === 0) {
-        minutes = "00";
-    }
-    else if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    
-    return minutes + ":" + seconds;
-});
+    // else{
+    //     $("#submitAnswers").on("click", stopClock);
+    // }
+}
 
 $("input").on("click", function (event){
     let optionValue = $(this).val();
@@ -64,28 +51,33 @@ $("input").on("click", function (event){
     }
 })
 
+
 $("#submitAnswers").on("click", function (event){
     event.preventDefault();
     $("#endResults").show();
     $("#startContainer").hide();
     $("#endResults").text("You guessed " + correctAnswers + " questions correctly!");
+
     console.log(event);
     console.log(correctAnswers);
     console.log(wrongAnswers);
+
+    clearInterval(intervalId);
+
     // alert("You guessed " + correctAnswers + " questions correctly!")
 
-});
+})
 
 
-
-
-
-
-
-// function endGame() {
-//     //check the answers for each option and see if it's right or wrong. 
-
+function endGame() {
+    //check the answers for each option and see if it's right or wrong. 
+    clearInterval(intervalId);
+    alert("Time's Up!");
     
+}
+
+// function stopClock() {
+//     clearInterval(intervalId);
 // }
 
 // $("#endGame").on("click", function (event){
@@ -107,24 +99,23 @@ $("#submitAnswers").on("click", function (event){
 //     console.log(converted);
     
 //     // DONE: Use the variable we just created to show the converted time in the "display" div.
-//     $("#timer").text(converted);
+//     $("#display").text(converted);
 //     }
-
 //     function timeConverter(t) {
 
 //         var minutes = Math.floor(t / 60);
 //         var seconds = t - (minutes * 60);
-        
+      
 //         if (seconds < 10) {
-//             seconds = "0" + seconds;
+//           seconds = "0" + seconds;
 //         }
-        
+      
 //         if (minutes === 0) {
-//             minutes = "00";
+//           minutes = "00";
 //         }
 //         else if (minutes < 10) {
-//             minutes = "0" + minutes;
+//           minutes = "0" + minutes;
 //         }
-        
+      
 //         return minutes + ":" + seconds;
-    // });
+//       }
